69b44a9301963a94b821706e31169d2e9675fdd8
Ali-Raza-Memon <alirazamemon0570@gmail.com>
2023-06-12 20:50:08 +0500

    Added Customized method

--- src/main/java/com/derivedmethods/repository/LaptopRepository.java
+++ src/main/java/com/derivedmethods/repository/LaptopRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LaptopRepository extends JpaRepository<Laptop,Integer> {
-     findByName(String name);
+     public Laptop findByName(String name);
 }
